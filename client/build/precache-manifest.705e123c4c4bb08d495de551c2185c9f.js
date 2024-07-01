91d9c0af609f8d88b61808b6cb3cbdbe1a5e2e18
wicked-aashcharya <aashcharyakumargupta@gmail.com>
2024-07-01 23:47:05 +0530

    new

---
+++ client/build/precache-manifest.705e123c4c4bb08d495de551c2185c9f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71205fb40f311002a608cfe3b8d15b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcab29eef27c96039436",
+    "url": "/static/css/main.f91fb546.chunk.css"
+  },
+  {
+    "revision": "ffacc246ff512e629403",
+    "url": "/static/js/2.91606790.chunk.js"
+  },
+  {
+    "revision": "fcab29eef27c96039436",
+    "url": "/static/js/main.63f8ca55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e839bf9f7b88bb24b5539f8b16b6df83",
+    "url": "/static/media/banner.8a9f498b.e839bf9f.webp"
+  },
+  {
+    "revision": "b8c79f033e8c7d1ca35dfc9261406fab",
+    "url": "/static/media/clerk-dark-logo.891b17ea.b8c79f03.webp"
+  },
+  {
+    "revision": "b111baaaa8b83197faaab9c65a3745d1",
+    "url": "/static/media/eraser-dark-logo.0ccbd449.b111baaa.webp"
+  },
+  {
+    "revision": "e26ec835e97c676e4b98ce367fba6e36",
+    "url": "/static/media/hand.e26ec835.webp"
+  },
+  {
+    "revision": "46a00b0745e3fc44978fbaff3c80775e",
+    "url": "/static/media/upstash-dark-logo.e96a691e.46a00b07.webp"
+  }
+]);
