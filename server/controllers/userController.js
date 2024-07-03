541d6c0a472240b15ebdaf2ff6d63f2157421114
Aashcharya Kumar Gupta <aashcharyakumargupta@gmail.com>
2024-07-03 13:41:17 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,41 @@ const bcrypt = require('bcrypt')
  
 /*This controller is sen to the userRoute.js */
 
+
+
+
+
+const resetPassword = async (req, res) => {
+  try {
+    const { email, newPassword } = req.body;
+
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Hash the new password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(newPassword, salt);
+
+    // Update the user's password
+    user.password = hashPassword;
+    await user.save();
+
+    const updatedUser = await User.findOne({ email });
+    if (updatedUser.password !== hashPassword) {
+      throw new Error("Password update failed");
+    }
+
+    res.status(200).json({ message: "Password reset successful" });
+  } catch (error) {
+    console.error("Error resetting password:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while resetting password" });
+  }
+};
 /* Sign up */
 
 
@@ -49,4 +84,4 @@ const login = async (req, res) => {
      }
 }
 
-module.exports = {signup, login}
+module.exports = {signup, login, resetPassword}
